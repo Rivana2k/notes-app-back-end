4b33d1dbf9e50d8e950761acdb155926bbac5946
Rivana2k <150756233+Rivana2k@users.noreply.github.com>
2023-11-16 07:59:21 +0700

    commit handlerjs
    menentukan newNote masuk ke dalam array notes berdasarkan id catatan

--- src/handler.js
+++ src/handler.js
@@ -13,6 +13,8 @@ const addNoteHandler = (request, h) => {
         };
 
         notes.push(newNote);
+
+        const isSuccess = notes.filter((note) => note.id === id).length > 0;
 };
 
 module.exports = { addNoteHandler };
